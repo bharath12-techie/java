56ecfe51b576374c0350292e3ef7bf1d36aa15d5
bharath12-techie <bharathkumar17124@gmail.com>
2024-10-01 11:09:03 +0100

    pushing code

---
+++ TotalPrice.java
@@ -0,0 +1,13 @@
+public class TotalPrice{
+
+
+  public static void main(String args[]){
+  String productName="mobile";
+  int price=10000;
+  int numberofProducts=10;
+  int totalprice=price*numberofProducts;
+  System.out.println("product name:"+productName);
+  System.out.println("total price is:"+totalprice);
+  }
+
+}
